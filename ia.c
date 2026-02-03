b2ffcde1ea6b45842697cdcf82f42ba232fa71dc
oscarjimenezpuig <44938488+oscarjimenezpuig@users.noreply.github.com>
2026-02-03 14:37:07 +0100

    Add files via upload

--- ia.c
+++ ia.c
@@ -20,6 +20,7 @@ typedef struct {
     visual_t vis[MAPAR][MAPAC]; //guarda todos los datos de la visualizacion
     int ri,ci; //extremo superior izquierdo de la parte visual
     int rf,cf;  //extremo inverior derecho de la parte visual
+    uint npc;
     objeto_t* obj;//objeto mas cercano de la visual 
     Bool jug; //el jugador existe en la visual
 } vismap_t;
@@ -28,7 +29,10 @@ typedef struct {
 
 vismap_t vismap;
 static Bool isobjnpc(objeto_t* o) {
-    if(o && o->npc && o->vid>0 && vismap.vis[o->r][o->c].fre==1) return TRUE;
+    if(o && o->npc && o->vid>0 && o->jug==0 && vismap.vis[o->r][o->c].fre==1) {
+        vismap.npc++;
+        return TRUE;
+    }
     return FALSE;
 }
 static Bool isobjnonpc(objeto_t* o) {
@@ -50,6 +54,7 @@ static vismap_t _visset(objeto_t* e) {
     vm.ci=MAPAC-1;
     vm.obj=NULL;
     vm.jug=FALSE;
+    vm.npc=0;
     /* coloca las partes transitables del mapa de visibilidad */
     localidad_t* le=mapget(e->r,e->c);
     if(le && le->osc) {
@@ -109,37 +114,36 @@ static vismap_t _visset(objeto_t* e) {
         objeto_t* npc=obj[k];
         vm.vis[npc->r][npc->c].fre=0;
     }
-    /* da el objeto mas cercano (comprueba si esta vestido con armadura) */
-    objeto_t* eprt=objisprt(e);
-    uint nits=objfnd(obj,isobjnonpc);
-    int dr=-1;
-    objeto_t* ite=NULL;
-    for(int k=0;k<nits;k++) {
-        objeto_t* itm=obj[k];
-        if(itm->prt==0 || (eprt==NULL)) {
-            int ditm=objdis(e,itm);
-            if(dr==-1 || (ditm<dr)) {
-                ite=itm;
-                dr=ditm;
+    /* da el objeto mas cercano (comprueba si esta vestido con armadura y si tiene espacio) */
+    uint invs=objinv(e,obj);
+    if(invs<e->cap) {
+        objeto_t* eprt=objisprt(e);
+        uint nits=objfnd(obj,isobjnonpc);
+        int dr=-1;
+        objeto_t* ite=NULL;
+        for(int k=0;k<nits;k++) {
+            objeto_t* itm=obj[k];
+            if(itm->prt==0 || (eprt==NULL)) {
+                int ditm=objdis(e,itm);
+                if(dr==-1 || (ditm<dr)) {
+                    ite=itm;
+                    dr=ditm;
+                }
             }
         }
-    }
-    if(ite) {
-        vm.obj=ite;
-        vm.vis[ite->r][ite->c].obj=1;
+        if(ite) {
+            vm.obj=ite;
+            vm.vis[ite->r][ite->c].obj=1;
+        }
     }
     /* retorno del resultado */
     return vm;
 }
 
-static void _moveini(objeto_t* e) {
-    /* inicia el movimiento de un objeto */
-    e->dr=e->dc=-1;
-}
-
 #define con(A) (DRCS-1-(A))
 
 static Bool _moveto(objeto_t* e,vismap_t vm,int rf,int cf) {
+    //dbgprt("move %s from %i,%i to %i,%i",e->nom,e->r,e->c,rf,cf);//dbg
     const int DR[]=DRC;
     int ri=e->r;
     int ci=e->c;
@@ -150,16 +154,19 @@ static Bool _moveto(objeto_t* e,vismap_t vm,int rf,int cf) {
     for(int k=0;k<DRCS;k++) {
         int dr=ri+DR[k];
         int dc=ci+DR[con(k)];
+        //dbgprt("propuesta %i,%i",dr,dc);//dbg
         if(vm.vis[dr][dc].fre==1 && (dr!=ra || dc!=ca)) {
+            //dbgprt("aceptada");//dbg
             int die=mapdis(dr,dc,rf,cf);
             if(dis==-1 || die<dis || (die==dis && rnd(0,1))) {
+                //dbgprt("pasa filtro con die=%i (dis=%i)",die,dis);//dbg
                 dis=die;
                 par=k;
             }
         }
     }
     int desr,desc;
-    desr=desc=-1;
+    desr=desc=0;
     if(par!=-1) {
         desr=DR[par];
         desc=DR[con(par)];
@@ -167,7 +174,8 @@ static Bool _moveto(objeto_t* e,vismap_t vm,int rf,int cf) {
         desr=SGN(ra,ri);
         desc=SGN(ca,ci);
     }
-    if(desr!=-1 && desc!=-1) {
+    //dbgprt("su move con par %i es %i,%i",par,desr,desc);//dbg
+    if(desr!=0 || desc!=0) {
         if(objmov(e,desr,desc)) {
             e->dr=ri;
             e->dc=ci;
@@ -219,11 +227,153 @@ static Bool _farjug(objeto_t* e,vismap_t vm,int* r,int* c) {
         }
     }
     return (dis!=-1);
-}  
+}
+
+static Bool _toobj(vismap_t vm,int* r,int* c) {
+    /* da las coordenadas del objeto mas cercano (si existe) */
+    if(vm.obj) {
+        *r=vm.obj->r;
+        *c=vm.obj->c;
+        return TRUE;
+    }
+    return FALSE;
+}
+
+static int _atabal(objeto_t* e) {
+	/* evalua las probabilidades de lucha cuerpo a cuerpo con el jugador */
+	/* si el resultado es positivo, es favorable a e, si es negativo, es favorable a jugador */
+	/* se tiene en cuenta la velocidad añadiendo un factor de proporcionalidad */
+	int danj=0;
+	int dane=0;
+	int fj,fe;
+	fj=fe=1;
+	if(e->vel>jugador->vel) fe=(e->vel+1)/(jugador->vel+1);
+	else if(e->vel<jugador->vel && fe) fj=(jugador->vel+1)/(e->vel+1);
+	for(int k=0;k<ATS;k++) {
+		if(HAT(e,jugador)) dane+=fe*DAN(e);
+		if(HAT(jugador,e)) danj+=fj*DAN(jugador);
+	}
+	return (dane-danj);
+}
+
+static double _armval(objeto_t* e,objeto_t*a) {
+    /* da una puntuacion al arma de ataque a partir de las caracteristicas */
+	const double MD[]={0.0,2.5,3.5,4.5,6.5,10.5};
+	double hab=e->hab+(a->pha==0)?0:(e->hab/a->pha);
+	double fue=e->fue+(a->pfu==0)?0:(e->fue/a->pfu);
+	double dad=MD[a->dad]*(double)a->nad;
+	return hab+fue+dad;
+}
+
+/* funciones principales de ia */
+
+static Bool iaatc(vismap_t vm,objeto_t* e) {
+    /* gestiona el ataque de un enemigo si el jugador esta presente */
+    /* res={0: nada, 1: acercarse 2: alejarse 3: atacar} */
+    int res=0;
+    if(vm.jug) {
+        uint dje=objdis(e,jugador);
+        if(e->anm) {
+            if(dje==1) res=3;
+            else res=1;
+        } else {
+            int atsim=_atabal(e);
+            if(atsim>0 || vm.npc>1) {
+               if(dje==1) res=3;
+                else res=1;
+            } else {
+                if(dje>1 || (dje==1 && e->vel>jugador->vel)) res=2;
+                else res=3;
+            }
+        }
+        int r,c;
+        if(res==1 && _nearjug(vm,&r,&c)) return _moveto(e,vm,r,c);
+        else if(res==2 && _farjug(e,vm,&r,&c)) return _moveto(e,vm,r,c);
+        else if(res==3) return objata(e,jugador);
+    }
+    return FALSE;
+}
+  
+static Bool iacog(vismap_t vm,objeto_t* e) {
+    /* intenta coger un objeto */
+    objeto_t* oc=vm.obj;
+    if(oc) {
+        if(oc->r==e->r && oc->c==e->c) return objcog(e,oc);
+        else {
+            int r,c;
+            if(_toobj(vm,&r,&c)) return _moveto(e,vm,r,c);
+        }
+    }
+    return FALSE;
+}
+
+static Bool iavst(objeto_t* e) {
+    /* intenta vestir un objeto */
+    objeto_t* inv[objsiz()];
+    uint invs=objinv(e,inv);
+    double ppoav=0;
+    double ppoad=0;
+    objeto_t* poad=NULL;
+    objeto_t* poav=NULL;
+    for(int k=0;k<invs;k++) {
+        objeto_t* oe=inv[k];
+        if(oe->arm) {
+            double pun=_armval(e,oe);
+            if(oe->ves) {
+                poad=oe;
+                ppoad=pun;
+            } else {
+                if(ppoav<pun) {
+                    ppoav=pun;;
+                    poav=oe;
+                }
+            }
+        }
+    }
+    if(poad && ppoad<ppoav) {
+        return objdes(e,poad);
+    } else if(!poad && poav) {
+        return objves(e,poav);
+    }
+    return FALSE;
+}
+    
+static Bool iawlk(vismap_t vm,objeto_t* e) {
+    static int rndr=-1;
+    static int rndc=-1;
+    if(rndr==-1 && rndc==-1) {
+        maprndpos(&rndr,&rndc,TRUE);
+    }
+    if((e->r==rndr && e->c==rndc) || (!_moveto(e,vm,rndr,rndc))) {
+        rndr=rndc=-1;
+        return FALSE;
+    }
+    return TRUE;
+}
 
 /* acciones de ia */
 
+Bool iaanm(objeto_t* e) {
+    vismap_t vm=_visset(e);
+    if(!iaatc(vm,e)) {
+        return iawlk(vm,e);
+    }
+    return TRUE;
+}
 
+Bool iahum(objeto_t* e) {
+    vismap_t vm=_visset(e);
+    if(!iaatc(vm,e)) {
+        if(!iacog(vm,e)) {
+            if(!iavst(e)) {
+                return iawlk(vm,e);
+            }
+        }
+    }
+    return TRUE;
+}
+         
+    
 
     
 
